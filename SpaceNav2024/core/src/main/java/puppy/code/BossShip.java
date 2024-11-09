48aa1d0a8487a6df53e590564ced47fd64ac427c
Marti <martina.sandoval.o@mail.pucv.cl>
2024-11-09 01:49:09 -0300

    pequeños cambios

--- SpaceNav2024/core/src/main/java/puppy/code/BossShip.java
+++ SpaceNav2024/core/src/main/java/puppy/code/BossShip.java
@@ -2,6 +2,7 @@ package puppy.code;
 
 import java.util.Random;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -9,7 +10,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
 
 public class BossShip extends SpaceShip {
-    private int bulletSpeed = 4;
+    private int bulletSpeed = 7;
     private int attackCounter = 0;
     private Sprite sprBoss;
 
@@ -20,12 +21,13 @@ public class BossShip extends SpaceShip {
         super(x, y, tx, txBullet, soundBullet);
         sprBoss = new Sprite(tx);
         sprBoss.setBounds(x, y, 80, 80);
-        setLifes(25);
+        setLifes(50);
+        //setMovementSpeed(2);
         attackPatterns = new Array<>();
-        attackPatterns.add(new CircularAttackPattern(10));
-        attackPatterns.add(new SpiralAttackPattern(3, 10));
-        attackPatterns.add(new WaveAttackPattern(10));
-        attackPatterns.add(new SweepAttackPattern(0, 180, 10));
+        attackPatterns.add(new CircularAttackPattern(12));
+        attackPatterns.add(new SpiralAttackPattern(3, 12));
+        attackPatterns.add(new WaveAttackPattern(12));
+        attackPatterns.add(new SweepAttackPattern(0, 180, 12));
         attackPatterns.add(new StarAttackPattern());
 
         selectRandomAttackPattern();
@@ -36,7 +38,7 @@ public class BossShip extends SpaceShip {
         float x = getX();
         float y = getY();
 
-        getSprite().setPosition(x + getXVel(), y + getYVel());
+        //movement();
         getSprite().draw(batch);
 
         attack(juego);
@@ -61,6 +63,11 @@ public class BossShip extends SpaceShip {
         } else
             setCooldown(getCooldown() - 0.45f);
     }
-
+    /*private void movement() {
+        setXVel(getMovementSpeed());
+        if (getX() + getXVel() < 0 || getX() + getXVel() + getSprite().getWidth() > Gdx.graphics.getWidth())
+            setMovementSpeed(getMovementSpeed() * -1);
+        getSprite().setPosition(getX() + getXVel(), getY());
+    }*/
 }
 
